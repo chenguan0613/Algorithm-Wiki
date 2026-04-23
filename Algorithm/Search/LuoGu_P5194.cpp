cddb25a0f7f17168714c87eaad053a1f27e6dd7e
ChenGuan <3168966792@qq.com>
2026-04-23 23:59:01 +0800

    searching

--- Algorithm/Search/LuoGu_P5194.cpp
+++ Algorithm/Search/LuoGu_P5194.cpp
@@ -3,26 +3,33 @@ using namespace std;
 typedef long long ll;
 ll n,c;
 vector<ll> w;
-vector<ll> post_sum;
+vector<ll> pre_sum;
 ll ans=0;
 void dfs(int u,ll current_w){
     if(current_w>c) return;
-    if(u==n || current_w==c){
+    if(u<0 || current_w==c){
         ans=max(ans,current_w);
         return;
     }
-    if(current_w+post_sum[u]<=ans) return;
-    dfs(u+1,current_w);
-    if(current_w+w[u]<=c) dfs(u+1,current_w+w[u]);
+    if(current_w+pre_sum[u+1]<=c){
+        ans=max(ans,current_w+pre_sum[u+1]);
+        return;
+    }
+    dfs(u-1,current_w);
+    if(current_w+w[u]<=c){
+        dfs(u-1,current_w+w[u]);
+    }
 }
 int main(){
     cin>>n>>c;
     w.resize(n);
-    post_sum.resize(n+1);
-    post_sum[n]=0;
-    for(int i=0;i<n;i++) cin>>w[i];
-    for(int i=n-1;i>=0;i--) post_sum[i]=post_sum[i+1]+w[i];
-    dfs(0,0);
+    pre_sum.resize(n+1);
+    pre_sum[0]=0;
+    for(int i=0;i<n;i++){
+        cin>>w[i];
+        pre_sum[i+1]=pre_sum[i]+w[i];
+    }
+    dfs(n-1,0);
     cout<<ans<<endl;
     return 0;
 }
